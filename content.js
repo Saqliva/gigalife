290bba8f1f70c05be508b04412d09f477ca2d155
Kiyotaka H. (3qr, Akai) <saqra23saqliva@gmail.com>
2024-12-02 04:23:13 +0900

    Add files via upload

---
+++ content.js
@@ -0,0 +1,4 @@
+const lifetime_element = document.getElementById("file_lifetime");
+lifetime_element.value = "100";
+const lifetime_divisions = document.getElementById("lifetime_divisions");
+lifetime_divisions.children[lifetime_divisions.children.length - 1].click();
